91e3746a53bb38e4c7c2c9c594b94874657491ec
nina-ezequiel <ninaezequiel20@gmail.com>
2026-06-14 15:26:20 -0300

    Update AF_Operations.h

--- Trabajo Practico Transversal/Parte3/AF_Operations.h
+++ Trabajo Practico Transversal/Parte3/AF_Operations.h
@@ -1,1 +1,19 @@
+#ifndef AF_OPERATIONS_H
+#define AF_OPERATIONS_H
 
+#include "AF.h"
+
+// Parsea una linea de texto (str) en una estructura DeltaEntry.
+// Formato: "origen,simbolo,destino" o "origen,simbolo,dest1,dest2,...,dest_n".
+DeltaEntry parseDeltaEntry(str linea);
+
+// Fusiona una nueva transicion en un arreglo dinamico de transiciones.
+void addTransition(DeltaEntry** delta, int* deltaCount, DeltaEntry newEntry);
+
+// Lee interactivamente transiciones y estados finales desde teclado.
+void readTransitions(DeltaEntry** delta, int* deltaCount, tData Q_set, tData Sigma_set, State* q0, tData* F);
+
+// Imprime el automata en la salida estandar.
+void printAF(const Af af);
+
+#endif
